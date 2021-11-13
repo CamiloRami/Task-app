4476d2b14a41c1dc236b722cdecd23c811663fd2
CamiloRami <camiloram18042003@gmail.com>
2021-11-13 10:08:10 -0300

    TodoList ahora es un componente que funciona con render props

--- src/App/index.js
+++ src/App/index.js
@@ -35,17 +35,21 @@ function App() {
           totalTodos={totalTodos}
           completedTodos={completedTodos}
         />
+
         <TodoSearch 
           searchValue={searchValue}
           setSearchValue={setSearchValue}
         />
       </TodoHeader>
-      <TodoList>
-        {error && <TodosError />}
-        {loading && <TodosLoading />}
-        {(!loading && !searchedTodos.length) && <EmptyTodos />}
-        
-        {searchedTodos.map(todo => (
+
+      <TodoList 
+        error={error}
+        loading={loading}
+        searchedTodos={searchedTodos}
+        onError={() => <TodosError />}
+        onLoading={() => <TodosLoading />}
+        onEmptyTodos={() => <EmptyTodos />}
+        render={todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
@@ -53,8 +57,8 @@ function App() {
             onComplete={() => completeTodoSwitch(todo.text)}
             onDelete={() => deleteTodo(todo.text)}
           />
-        ))}
-      </TodoList>
+        )}
+      />
 
       {openModal && (
         <Modal>
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -3,7 +3,14 @@ import './TodoList.css'
 
 function TodoList(props) {
   return (
-    <section>
+    <section className="TodoList-container">
+      {props.error && props.onError()}
+      {props.loading && props.onLoading()}
+
+      {(!props.loading && !props.searchedTodos.length) && props.EmptyTodos()}
+
+      {props.searchedTodos.map(props.render)} {/*todo => props.render(todo)  y props.render  hacen la misma funcion como paramentro, pero mas corto */}
+
       <ul>
         {props.children}
       </ul>
